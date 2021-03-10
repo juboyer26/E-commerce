e226be9985e8506e15b746e51417c1929591d207
juboyer26 <juboyer@student.wethinkcode.co.za>
2021-03-10 16:07:03 +0200

    update menu-items to navigate withRouter

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
+
 import Homepage from './pages/homepage/homepage.component'
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={HatsPage} />
     </div>
   );
 }
 
 export default App;
+ 
